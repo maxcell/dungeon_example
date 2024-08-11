a8f323962185111df41be9296fa486b1e830e26c
Prince Wilson <maxcell.wilson@gmail.com>
2024-08-11 14:54:47 -0400

    Adds tilemap integration and zooms camera out

--- src/map.rs
+++ src/map.rs
@@ -1,4 +1,5 @@
 use bevy::{prelude::*};
+use bevy_ecs_tilemap::{map::{TilemapId, TilemapSize, TilemapTexture, TilemapTileSize, TilemapType}, prelude::get_tilemap_center_transform, tiles::{TileBundle, TilePos, TileStorage, TileTextureIndex}, TilemapBundle};
 
 use crate::assets::ImageAssets;
 
@@ -24,18 +25,52 @@ impl Map {
 }
 
 pub fn spawn_world(mut commands: Commands, my_assets: Res<ImageAssets>) {
+
   let map = Map::new(30);
 
-  let mut texture_atlas = TextureAtlas::from(my_assets.layout.clone());
-  texture_atlas.index = 14;
+  // Tile counts
+  let map_size = TilemapSize { x: 60, y: 30 };
 
-  commands.insert()
+  // Where we will store the tilemap entity
+  let tilemap_entity = commands.spawn_empty().id();
+
   
-  // commands.spawn((SpriteBundle {
-  //     texture: my_assets.ground.clone(),
-  //     // transform: Transform::from_xyz(0.0, -64.0, 0.0),
-  //   ..default()
-  // }, texture_atlas));
+  let mut tile_storage = TileStorage::empty(map_size);
+
+  for x in 0..map_size.x {
+    for y in 0..map_size.y {
+      let tile_pos = TilePos { x, y };
+      let tile_entity = commands
+        .spawn(TileBundle {
+          position: tile_pos,
+          tilemap_id: TilemapId(tilemap_entity),
+          // Texture Atlas index
+          texture_index: TileTextureIndex(14 as u32),
+          ..Default::default()
+        }
+      )
+      .id();
+
+    tile_storage.set(&tile_pos, tile_entity);
+        
+    }
+  }
+
+  // Actual map size by a given tile count
+  let tile_size = TilemapTileSize { x: 64., y: 64. };
+  let grid_size = tile_size.into();
+  let map_type = TilemapType::default();
+
+  commands.entity(tilemap_entity).insert(TilemapBundle {
+    grid_size,
+    map_type,
+    size: map_size,
+    storage: tile_storage,
+    texture: TilemapTexture::Single(my_assets.ground.clone()),
+    tile_size,
+    transform: get_tilemap_center_transform(&map_size, &grid_size, &map_type, 0.0),
+    ..Default::default()
+  });
 }
 
 
@@ -43,5 +78,9 @@ pub fn spawn_world(mut commands: Commands, my_assets: Res<ImageAssets>) {
 pub struct MyGameCamera;
 
 pub fn spawn_camera(mut commands: Commands) {    
-    commands.spawn(Camera2dBundle::default());
+  let mut camera =  Camera2dBundle {
+    ..Default::default()
+  };
+  camera.projection.scale *= 2.5;
+    commands.spawn(camera);
 }
